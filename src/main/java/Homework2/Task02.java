ce6ad8898fc2e3d92d017d01e0c5aa585f718873
Petya Todorova <petiatodorova95@gmail.com>
2023-05-21 13:07:42 +0300

    Adding a readme

---
+++ src/main/java/Homework2/Task02.java
@@ -0,0 +1,12 @@
+package Homework2;
+
+public class Task02 {
+    public static void arithmeticValue(int a, int b) {
+        int value = (a + b) / 2;
+        System.out.println("Arithmetic value of " + a + " and " + b + " is " + value);
+    }
+    public static void main(String[] args) {
+        arithmeticValue(10, 20);
+
+    }
+}
