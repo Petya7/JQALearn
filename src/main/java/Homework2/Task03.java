2079e1c57dfd0632672645e19cd048dd03683e1a
Petya Todorova <petiatodorova95@gmail.com>
2023-05-21 14:03:59 +0300

    Adding a readme

---
+++ src/main/java/Homework2/Task03.java
@@ -0,0 +1,31 @@
+package Homework2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task03 {
+    public static void main(String[] args) {
+        double[] arr = {1, 2, 3, 4, 5};
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+    }
+}
+
+
+//public class Task03 {
+//    public static void main(String[] args) {
+//        double[] arr = {1, 2, 3, 4, 5};
+//        System.out.println(Arrays.toString(arr));
+//    }
+//}
+
+
+
+
+
+
+
+
