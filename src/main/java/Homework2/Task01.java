6e998e63450fe33ef770befbaf06e4f046ed7c08
Petya Todorova <petiatodorova95@gmail.com>
2023-05-21 12:57:45 +0300

    Adding a readme

---
+++ src/main/java/Homework2/Task01.java
@@ -0,0 +1,14 @@
+package Homework2;
+
+import java.util.Scanner;
+
+public class Task01 {
+    public static void sumTwo(int a, int b) {
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is " + sum);
+    }
+    public static void main(String[] args) {
+        sumTwo(10, 5);
+
+    }
+}
