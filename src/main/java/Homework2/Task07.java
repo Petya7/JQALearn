e7ec26036de8758db9bf4917b1575b706651a1cf
Petya Todorova <petiatodorova95@gmail.com>
2023-05-23 19:22:43 +0300

    Adding readme

---
+++ src/main/java/Homework2/Task07.java
@@ -0,0 +1,17 @@
+package Homework2;
+
+
+    public class Task07 {
+        public static void main(String[] args) {
+            int number1 = 5;
+            int number2 = 10;
+            int result = multiply(number1, number2);
+            System.out.println("The result of multiplication is: " + result);
+        }
+
+        public static int multiply(int num1, int num2) {
+            return num1 * num2;
+        }
+    }
+
+
